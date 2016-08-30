8e371c183281713df941ca372b1719d0ec922bfc
IMLaing <ianmlaing@gmail.com>
2016-08-30 18:44:50 -0400

    added to ghpages

--- AAAPI.js
+++ AAAPI.js
@@ -1,16 +1,45 @@
-var geocoder;
-var map;
+var geocoder, AAMap, volunteerMap;
+
+function geoCheck(){
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        AAMap.setCenter({lat:position.coords.latitude, lng:position.coords.longitude});
+        volunteerMap.setCenter({lat:position.coords.latitude, lng:position.coords.longitude});
+      });
+    } else {
+      console.log('no geolocation is available');
+    }
+}
+
+geoCheck();
+
 function initMap() {
    geocoder = new google.maps.Geocoder();
-   var myLatLng = {lat: -34.397, lng: 150.644};
+   var myLatLng = {lat: 42.36, lng: -71.06};
    var mapOptions = {
     zoom: 8,
     center: myLatLng
   };
-  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  
+  AAMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+  volunteerMap = new google.maps.Map(document.getElementById('map2'), mapOptions);
+
+  $.getJSON('./geocoded.json', function(data){
+    data.forEach(function(element, index, array){
+    var AAMarker = new google.maps.Marker({
+        map: AAMap,
+        position: {lat: element.lat, lng:element.lng}
+      });
+    var volunteerMarker = new google.maps.Marker({
+        map: volunteerMap,
+        position: {lat: element.lat, lng:element.lng}
+      });
+    });
+  });
 }
 
-  function codeAddress(){
+
+/*=function codeAddress(){
     var address = document.getElementById('address').value;
     geocoder.geocode( {'address':address}, function(results, status) {
      if (status == 'OK') {
@@ -24,4 +53,4 @@ function initMap() {
     }
 
   });
-  }
+  }*/
--- geoMap.js
+++ geoMap.js
@@ -2,11 +2,12 @@ var googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyCZSztzrJQoCzboyW0zj-EwQqfYE7NvjfE'
 });
 
-var data = require('./locations.json');
+var data = require('./phone.json');
 data.forEach(function(element, index, array){
 	googleMapsClient.geocode({
 	  	address: element.venue+', '+element.city
 	}, 
+
 	function(err, response) {
   		if (!err) {
   			if(response.json.results.length > 0){
@@ -15,7 +16,8 @@ data.forEach(function(element, index, array){
 					venue: element.venue,
 					city: element.city,
 					lat: location.lat,
-					lng: location.lng
+					lng: location.lng,
+					phone: element.phone
 				});
 			} else {
 				console.error('no such location');
@@ -24,3 +26,11 @@ data.forEach(function(element, index, array){
 	});
 });
 // Geocode an address.
+/*
+
+var fs = require('fs');
+fs.readFile('/etc/passwd', function(err, buf) {
+  console.log(buf.toString());
+});
+
+*/
